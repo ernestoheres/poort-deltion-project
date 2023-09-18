cd0e3df2f057faf41745f523afcbfb74257092bb
W4STT3D <edgy.views@hotmail.com>
2023-09-18 13:21:11 +0200

    Table styling begin

--- frontend/src/components/table.vue
+++ frontend/src/components/table.vue
@@ -5,9 +5,9 @@
       <!-- Table header here -->
       <tbody>
         <tr v-for="user in users" :key="user.id">
-          <td>{{ user.voornaam }}</td>
-          <td>{{ user.achternaam }}</td>
-          <td>{{ user.adres }}</td>
+           <td>{{ user.voornaam }}</td>
+           <td>{{ user.achternaam }}</td>
+           <td>{{ user.adres }}</td>
         </tr>
       </tbody>
     </table>
@@ -36,6 +36,16 @@ export default {
 };
 </script>
 
-<style>
-/* Table styling here */
+<style scoped>
+
+table {
+  border: 3px solid grey;
+  box-shadow: 0 1px 1px rgba(0,0,0,0.15), 0 2px 2px rgba(0,0,0,0.15), 0 4px 4px rgba(0,0,0,0.15), 0 8px 8px rgba(0,0,0,0.15);
+  border-radius: 20px;
+}
+
+tr {
+  border: 3px solid grey;
+}
+
 </style>
