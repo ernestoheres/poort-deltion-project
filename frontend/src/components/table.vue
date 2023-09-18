ecada2647555604b18d7fe6f5d75149898c8f233
W4STT3D <edgy.views@hotmail.com>
2023-09-18 15:30:02 +0200

    more styling table

--- frontend/src/components/table.vue
+++ frontend/src/components/table.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <h1>Vue Table Example</h1>
     <table>
-      <!-- Table header here -->
       <tbody>
         <tr v-for="user in users" :key="user.id">
-           <td>{{ user.voornaam }}</td>
-           <td>{{ user.achternaam }}</td>
+           <td> 
+            <img src="../../public/joebiden.jpg" alt="User Image" class="user-image" />
+            {{ user.name }}
+            </td>
+            <tr>
+           <td>{{ user.voornaam }} {{ user.achternaam }}</td>
+           </tr>
+           <tr>
            <td>{{ user.adres }}</td>
+           </tr>
+
         </tr>
       </tbody>
     </table>
@@ -24,8 +30,7 @@ export default {
     };
   },
   mounted() {
-    // Make a GET request to your backend API to fetch the data
-    axios.get('http://localhost:8000/api/clients') // Replace with your backend API URL
+    axios.get('http://localhost:8000/api/clients')
       .then(response => {
         this.users = response.data;
       })
@@ -42,10 +47,20 @@ table {
   border: 3px solid grey;
   box-shadow: 0 1px 1px rgba(0,0,0,0.15), 0 2px 2px rgba(0,0,0,0.15), 0 4px 4px rgba(0,0,0,0.15), 0 8px 8px rgba(0,0,0,0.15);
   border-radius: 20px;
+  border-collapse: collapse;  
+}
+
+td {
+  
 }
 
 tr {
-  border: 3px solid grey;
+    height: 50px;
+}
+
+img {
+  width: 90px;
+  height: 110px;
 }
 
 </style>
