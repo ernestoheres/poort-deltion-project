1e9c873ee69d044fe0cb27301b375f4d62c746ea
thomas schoemaker <thomasdj100@gmail.com>
2023-09-20 13:00:36 +0200

    Denied user added

---
+++ frontend/src/components/denied.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <img src="../assets/logopoort2.png">
+    </div>
+    <div>
+        Inloggen niet mogelijk!
+    </div>
+    <div>
+        Uw accont is gedeactiveerd. Neem contact op met de klantenservice voor meer informatie.
+    </div>
+    <div>
+        <a href="tel:+310321329999">0321329999</a>
+    </div>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Table from '../components/table.vue'
+import Denied from '../components/denied.vue'
 </script>
 
 <template>
   <main>
+    <Denied />
     <Table />
   </main>
 </template>
