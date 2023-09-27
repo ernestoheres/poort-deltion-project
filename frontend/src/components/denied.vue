75d52239299e8ae3e6339940685d2e4c93048dd8
thomas schoemaker <thomasdj100@gmail.com>
2023-09-27 12:41:23 +0200

    Header

--- frontend/src/components/denied.vue
+++ frontend/src/components/denied.vue
@@ -1,18 +1,21 @@
 <template>
 <div class="Cont">
-    <div>
-        <img class="Foto" src="../assets/logopoort2.png" width="100px">
-    </div>
-    <div class="TextCont">
-    <div>
-        Inloggen niet mogelijk!
-    </div>
-    <div>
-        Uw accont is gedeactiveerd. Neem contact op met de klantenservice voor meer informatie.
-    </div>
-    <div>
-        <a class="TelNum" href="tel:+310321329999">0321329999</a>
-    </div>
+    <div class="BackGround">
+        <div>
+            <img class="Foto" src="../assets/logopoort2.png" width="100px">
+        </div>
+        <div class="TextCont">
+            <div>
+                Inloggen niet mogelijk!
+            </div>
+            <div>
+                Uw account is gedeactiveerd.<br>
+                Neem contact op met de klantenservice voor meer informatie.
+            </div>
+            <div>
+                <a class="TelNum" href="tel:+310321329999">Klantenservice: 0321329999</a>
+            </div>
+        </div>
     </div>
 </div>
 </template>
@@ -20,15 +23,35 @@
     .TelNum{
         color: black;
     }
+    .TelNum:hover{
+        background-color: rgba(211, 227, 233, 0.562);
+        color: black;
+    }
     .TextCont{
         display: flex;
         flex-direction: column;
     }
-    .Cont{
-        display: flex;
+    .BackGround{
         background-color: azure;
         color: black;
+        display: flex;
         border-radius: 15px;
+        width: 725px;
+        box-shadow: 0px 1px 20px 9px black;
+        left: 35%;
+        height: 125px;
+        align-items: center;
+        padding-left: 10px;
+        top: 45%;
+        position: relative;
+    }
+    .Cont{
+        left: 0%;
+        position: fixed;
+        height: 100%;
+        width: auto;
+        background-color: rgba(173, 216, 230, 0.322);
+        display: block;
     }
     .Foto{
         width: 100px;
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Table from '../components/table.vue'
 import Denied from '../components/denied.vue'
+import Header from '../components/header.vue'
 </script>
 
 <template>
   <main>
     <!-- <Denied /> -->
+    <Header />
     <Table />
   </main>
 </template>
