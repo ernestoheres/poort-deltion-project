3b58dc5d8faa20c5b4e443f6de5edfbefcf0c7a7
JanWillem <janwillem@cimiro.com>
2023-09-29 11:01:29 +0200

    git merge fixed, loginscreen aan homeview toegevoegd

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,21 +1,17 @@
 <script setup>
-<<<<<<< HEAD
-import LoginScreen from '../components/LoginScreen.vue'
-=======
 import Table from '../components/table.vue'
 import Denied from '../components/denied.vue'
+import LoginScreen from '../components/LoginScreen.vue'
 import Header from '../components/header.vue'
->>>>>>> 75d52239299e8ae3e6339940685d2e4c93048dd8
 </script>
 
 <template>
   <main>
-<<<<<<< HEAD
-    <LoginScreen />
-=======
     <!-- <Denied /> -->
+
+    <!-- <LoginScreen /> -->
+
     <Header />
     <Table />
->>>>>>> 75d52239299e8ae3e6339940685d2e4c93048dd8
   </main>
 </template>
