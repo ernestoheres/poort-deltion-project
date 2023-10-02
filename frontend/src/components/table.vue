1e724201a02ca50997ab5ba3b7839ea17525729b
JanWillem <janwillem@cimiro.com>
2023-10-02 15:40:13 +0200

    table styling

--- frontend/src/components/table.vue
+++ frontend/src/components/table.vue
@@ -1,25 +1,26 @@
 <template>
   <div id="table-container">
-    <table>
-      <tbody>
-        <tr v-for="user in users" :key="user.id">
-          <td> 
-            <img :src="'http://127.0.0.1:8000/api/clients/' + user.id + '/image'" alt="User Image" class="user-image">
-            {{ user.name }}
-          </td>
-          
-          <tr>
-           <td>{{ user.voornaam }} {{ user.achternaam }}</td>
+    <div class="table-border">
+      <table>
+        <tbody>
+          <tr v-for="user in users" :key="user.id" class="User-TR">
+            <td> 
+              <img :src="'http://127.0.0.1:8000/api/clients/' + user.id + '/image'" alt="User Image" class="user-image">
+              {{ user.name }}
+            </td>
+            
+            <tr>
+            <td>{{ user.voornaam }} {{ user.achternaam }}</td>
+            </tr>
+
+
+            <tr>
+            <td>{{ user.adres }}</td>
+            </tr>
           </tr>
-
-
-          <tr>
-           <td>{{ user.adres }}</td>
-          </tr>
-
-        </tr>
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -55,17 +56,29 @@ export default {
 }
 
 table {
-  border: 3px solid grey;
-  box-shadow: 0 1px 1px rgba(0,0,0,0.15), 0 2px 2px rgba(0,0,0,0.15), 0 4px 4px rgba(0,0,0,0.15), 0 8px 8px rgba(0,0,0,0.15);
-  border-radius: 20px;
-  border-collapse: collapse;  
+  width: 100%;
+  border-collapse: collapse;
+}
 
+.table-border {
+  border: 1px solid lightgray;
+  box-shadow: 0 0 5px 3px rgba(0,0,0,0.15);
+  border-radius: 15px;
+  padding: 10px 0; 
   width: 90%;
-  margin-top: 25px;
 }
 
 td {
   
+}
+
+.User-TR {
+  padding: 0 10px;
+  border-bottom: 1px solid lightgray;
+}
+
+.User-TR:last-child {
+  border-bottom: none;
 }
 
 tr {
@@ -73,8 +86,11 @@ tr {
 }
 
 img {
-  width: 110px;
-  height: 110px;
+  margin-top: 8px;
+  border-radius: 100%;
+  width: 75px;
+  height: 75px;
+  margin-left: 25px;
 }
 
 </style>
