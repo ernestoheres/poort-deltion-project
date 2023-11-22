a8fe817187841eaf72fd951dc44a8700ddc4d7d9
JanWillemBijker <janwillem@cimiro.com>
2023-11-22 17:19:13 +0100

    wanneer je niet bent ingelogd zie je alleen logo en button naar inlog pagina

--- frontend/src/components/sidebar.vue
+++ frontend/src/components/sidebar.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="Side" :class="{ 'sidebar-open': isSidebarOpen }" @click="toggleSidebar" >
+    <div class="Side" :class="{ 'sidebar-open': isSidebarOpen }" @click="toggleSidebar">
         <div class="top">
-            <div class="logo" >
+            <div class="logo">
                 <img href="./" class="Foto" src="../assets/logopoort2.png">
             </div>
-            <div class="close-sidebar" >
+            <div class="close-sidebar">
                 <i class="fa-regular fa-circle-chevron-left fa-xl" style="color:#9B1D20"></i>
             </div>
         </div>
 
-        <div class="center" @click="toggleSidebar" v-if="userRole === 'administrator' || userRole === 'doctor'" >
+        <div class="center" @click="toggleSidebar" v-if="userRole === 'administrator' || userRole === 'doctor'">
             <div class="icon">
                 <a href="/dashboard">
                     <i class="fa-regular fa-rectangle-list fa-2x icon-highlight"></i>
@@ -17,21 +17,21 @@
                 </a>
             </div>
 
-            <div class="icon" v-if="userRole === 'administrator' || userRole === 'doctor'" >
+            <div class="icon" v-if="userRole === 'administrator' || userRole === 'doctor'">
                 <a href="/dashboard/archive">
                     <i class="fa-light fa-box-archive fa-2x icon-highlight"></i>
                     <p>Archive</p>
                 </a>
             </div>
 
-            <div class="icon" v-if="userRole === 'administrator'" >
+            <div class="icon" v-if="userRole === 'administrator'">
                 <a href="/dashboard/user/add">
                     <i class="fa-regular fa-user-plus fa-2x icon-highlight"></i>
                     <p>Toevoegen</p>
                 </a>
             </div>
 
-            <div class="icon" v-if="userRole === 'administrator' || userRole === 'doctor'" >
+            <div class="icon" v-if="userRole === 'administrator' || userRole === 'doctor'">
                 <a href="/dashboard/calender">
                     <i class="fa-thin fa-calendar-days fa-2x icon-highlight"></i>
                     <p>Kalender</p>
@@ -40,10 +40,16 @@
         </div>
 
         <div class="bottom">
-            <div class="icon">
+            <div v-if="isTokenSet" class="icon">
                 <a href="/" @click.prevent="logout">
                     <i class="fa-solid fa-right-from-bracket fa-2x icon-highlight"></i>
                     <p style="text-wrap: nowrap;">Log uit</p>
+                </a>
+            </div>
+            <div v-else class="icon">
+                <a href="/">
+                    <i class="fa-solid fa-right-to-bracket fa-2x icon-highlight"></i>
+                    <p style="text-wrap: nowrap;">Log in</p>
                 </a>
             </div>
         </div>
@@ -54,165 +60,167 @@
 
 
 <script>
-import 'font-awesome/css/font-awesome.css'
+    import 'font-awesome/css/font-awesome.css'
 
-export default {
-  data() {
-    return {
-      isSidebarOpen: false,
-      userRole: '',
+    export default {
+        data() {
+            return {
+                isSidebarOpen: false,
+                userRole: '',
+            };
+        },
+        created() {
+            this.userRole = localStorage.getItem('role');
+            this.isTokenSet = !!localStorage.getItem('token');
+        },
+        methods: {
+            toggleSidebar() {
+                this.isSidebarOpen = !this.isSidebarOpen;
+            },
+            logout() {
+                localStorage.clear();
+
+                this.$router.push('/');
+            },
+        },
     };
-  },
-  created() {
-    this.userRole = localStorage.getItem('role');
-    console.log('Role:', this.userRole);
-  },
-  methods: {
-    toggleSidebar() {
-      this.isSidebarOpen = !this.isSidebarOpen;
-    },
-    logout() {
-      localStorage.removeItem('role');
-      localStorage.removeItem('userid');
-      localStorage.removeItem('token');
-
-      this.$router.push('/');
-    },
-  },
-};
 </script>
 
 
 <style scoped>
+    .logo {
+        cursor: pointer;
+    }
 
-.logo {
-    cursor: pointer;
-}
+    .logo img {
+        width: 47.5px;
+        height: auto;
+    }
 
-.logo img {
-    width: 47.5px;
-    height: auto;
-}
+    .close-sidebar {
+        cursor: pointer;
+        margin-right: 3px;
+    }
 
-.close-sidebar {
-    cursor: pointer;
-    margin-right: 3px;
-}
+    .Side .icon {
+        transition: background-color .3s, padding-left .3s;
+        border-radius: 15px;
+    }
 
-.Side .icon {
-    transition: background-color .3s, padding-left .3s;
-    border-radius: 15px;
-}
+    .Side.sidebar-open .icon {
+        padding: 8px 15px;
+    }
 
-.Side.sidebar-open .icon {
-    padding: 8px 15px;
-}
+    .Side.sidebar-open .icon:hover {
+        background-color: hsla(160, 100%, 37%, 0.15);
+    }
 
-.Side.sidebar-open .icon:hover {
-    background-color: hsla(160, 100%, 37%, 0.15);
-}
-
-.Side .icon:hover * {
-    filter: grayscale(50%);
-    cursor: pointer;
-    transition: all .3s;
-}
+    .Side .icon:hover * {
+        filter: grayscale(50%);
+        cursor: pointer;
+        transition: all .3s;
+    }
 
 
-.Item{
-    padding-inline: 15px;
-    padding-top: 15px;
-}
-.Side{
-    box-shadow: 0 0 15px rgba(0,0,0,0.5);
-    clip-path: inset(0px -15px 0px 0px);
-    background-color: white;
-    min-height: calc(100vh - 90px);
-    width: 75px;
-    left: 0;
-    align-items: flex-start;
-    color: black;
-    display: flex;
-    flex-direction: column;
-    top: 5;
-    z-index: 2;
-    justify-content: space-between;
-    padding: 15px;
-    transition: all .3s;
-    cursor: w-resize;
-}
+    .Item {
+        padding-inline: 15px;
+        padding-top: 15px;
+    }
 
-.Side .icon a  {
-    display: flex;
-    flex-direction: row;
-    width: fit-content;
-    gap: 0;
-    align-items: center;
-    flex-wrap: nowrap;
-}
+    .Side {
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
+        clip-path: inset(0px -15px 0px 0px);
+        background-color: white;
+        min-height: calc(100vh - 90px);
+        width: 75px;
+        left: 0;
+        align-items: flex-start;
+        color: black;
+        display: flex;
+        flex-direction: column;
+        top: 5;
+        z-index: 2;
+        justify-content: space-between;
+        padding: 15px;
+        transition: all .3s;
+        cursor: w-resize;
+    }
 
-.Side.sidebar-open .icon a p  {
-    gap: 10px;
-}
+    .Side .icon a {
+        display: flex;
+        flex-direction: row;
+        width: fit-content;
+        gap: 0;
+        align-items: center;
+        flex-wrap: nowrap;
+    }
 
-.Side .icon a p  {
-    width: 0px;
-    overflow: hidden;
-    transition: all .3s;
-    display: block;
-}
+    .Side.sidebar-open .icon a p {
+        gap: 10px;
+    }
 
-.Side.sidebar-open .icon a p  {
-    width: 80px;
-}
+    .Side .icon a p {
+        width: 0px;
+        overflow: hidden;
+        transition: all .3s;
+        display: block;
+    }
 
-.Side.sidebar-open {
-    width: 200px !important;
-}
+    .Side.sidebar-open .icon a p {
+        width: 80px;
+    }
 
-.Side.sidebar-open .icon a  {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    align-items: center;
-    gap: 15px;
-}
+    .Side.sidebar-open {
+        width: 200px !important;
+    }
 
-.Side.sidebar-open .icon a i  {
-    min-width: 40px;
-    width: 40px;
-    max-width: 40px;
-}
-.Side.sidebar-open .icon a p  {
-    display: block;
-}
+    .Side.sidebar-open .icon a {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        align-items: center;
+        gap: 15px;
+    }
 
-.top, .center, .bottom {
-    display: flex;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    gap: 30px;
-}
-.Side .top {
-    overflow: hidden;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    width: 65px;
-    justify-content: space-between;
-    align-items: center;
-    transition: all .3s;
-}
+    .Side.sidebar-open .icon a i {
+        min-width: 40px;
+        width: 40px;
+        max-width: 40px;
+    }
 
-.Side.sidebar-open .top {
-    width: 100%;
-}
+    .Side.sidebar-open .icon a p {
+        display: block;
+    }
 
-.Side.sidebar-open .center {
-    gap: 14px;
-    margin-bottom: -8px;
-}
+    .top,
+    .center,
+    .bottom {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        gap: 30px;
+    }
 
-.Side.sidebar-open .bottom {
-    margin-bottom: -8px;
-}
+    .Side .top {
+        overflow: hidden;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        width: 65px;
+        justify-content: space-between;
+        align-items: center;
+        transition: all .3s;
+    }
+
+    .Side.sidebar-open .top {
+        width: 100%;
+    }
+
+    .Side.sidebar-open .center {
+        gap: 14px;
+        margin-bottom: -8px;
+    }
+
+    .Side.sidebar-open .bottom {
+        margin-bottom: -8px;
+    }
 </style>
